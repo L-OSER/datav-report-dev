0ea17e09b334d7d41b7a7fedba72e213d97edd1a
zhoujj <zhoujj@dragoninfo.com.cn>
2022-07-11 18:11:09 +0800

    关键词

--- src/components/BottomView/index.vue
+++ src/components/BottomView/index.vue
@@ -1,12 +1,116 @@
 <template>
-  <div>BottomView</div>
+  <div class="bottom-view">
+    <div class="view">
+      <el-card shadow="hover">
+        <template v-slot:header>
+          <div class="title-wrapper">关键词搜索</div>
+        </template>
+        <template>
+          <div class="chart-wrapper">
+            <div class="chart-inner">
+              <div class="chart">
+                <div class="chart-title">搜索用户数</div>
+                <div class="chart-data">93,634</div>
+                <v-chart :options="searchUserOption"></v-chart>
+              </div>
+              <div class="chart">
+                <div class="chart-title">搜索量</div>
+                <div class="chart-data">198,732</div>
+                <v-chart :options="searchNumberOption"></v-chart>
+              </div>
+            </div>
+          </div>
+          <div class="table-wrapper">
+            <el-table :data="tableData">
+              <el-table-column></el-table-column>
+            </el-table>
+            <el-pagination></el-pagination>
+          </div>
+        </template>
+      </el-card>
+    </div>
+    <div class="view">
+      <el-card shadow="hover">
+        <template v-slot:header>
+          <div class="title-wrapper">
+            <div class="title">分类销售排行</div>
+            <div class="radio-wrapper">
+              <el-radio-group v-model="radioSelect" size="small">
+                <el-radio-button label="品类"> </el-radio-button>
+                <el-radio-button label="商品"> </el-radio-button>
+              </el-radio-group>
+            </div>
+          </div>
+        </template>
+        <template>
+          <v-chart :options="categoryOptions"></v-chart>
+        </template>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'BottomView'
+  name: 'BottomView',
+  data() {
+    return {
+      searchUserOption: {},
+      searchNumberOption: {},
+      tableData: [],
+      radioSelect: '品类',
+      categoryOptions: {}
+    }
+  }
 }
 </script>
-
-<style>
+<style lang="scss" scoped>
+.bottom-view {
+  display: flex;
+  margin-top: 20px;
+  .view {
+    flex: 1;
+    width: 50%;
+    box-sizing: border-box;
+    &:first-child {
+      padding: 0 10px 0 0;
+    }
+    &:last-child {
+      padding: 0 0 0 10px;
+    }
+    .title-wrapper {
+      display: flex;
+      align-items: center;
+      height: 60px;
+      box-sizing: border-box;
+      border-bottom: 1px solid #eee;
+      font-size: 14px;
+      font-weight: 500;
+      padding: 0 0 0 20px;
+      .radio-wrapper {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+        padding-right: 20px;
+      }
+    }
+    .chart-wrapper {
+      display: flex;
+      flex-direction: column;
+      height: 452px;
+      .chart-inner {
+        display: flex;
+        padding: 0 10px;
+        margin-top: 20px;
+        .chart {
+          flex: 1;
+          .chart-title {
+          }
+          .chart-data {
+          }
+        }
+      }
+    }
+  }
+}
 </style>
